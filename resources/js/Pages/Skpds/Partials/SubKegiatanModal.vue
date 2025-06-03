ce609ee021dbcea8e83a1021dced3da66afff017
agungsetiob <agungsetiobudi19@gmail.com>
2025-06-03 14:06:08 +0800

    add opacity

--- resources/js/Pages/Skpds/Partials/SubKegiatanModal.vue
+++ resources/js/Pages/Skpds/Partials/SubKegiatanModal.vue
@@ -41,7 +41,7 @@
               </button>
               <button
                 type="submit"
-                class="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors"
+                class="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors disabled:opacity-50"
                 :disabled="form.processing"
               >
                 <span v-if="form.processing">
