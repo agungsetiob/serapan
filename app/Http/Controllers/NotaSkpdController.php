c36dd0a2442acceb123c344e5fbc32d1f38bf2fb
Agung Setio Budi <agungsetiobudi19@gmail.com>
2025-07-29 14:07:34 +0800

    fix bug

--- app/Http/Controllers/NotaSkpdController.php
+++ app/Http/Controllers/NotaSkpdController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\NotaSkpdRequest;
 use App\Models\NotaDinas;
-use App\Models\SKPD;
+use App\Models\Skpd;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
