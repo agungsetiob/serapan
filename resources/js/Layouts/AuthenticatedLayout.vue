53b5cd35ebabf232921c3910faac3719f6394b3a
agungsetiob <agungsetiobudi19@gmail.com>
2025-07-31 14:56:40 +0800

    fix authenticated layout

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -52,7 +52,7 @@ function isActive(routeName) {
           <div class="flex">
             <div class="flex shrink-0 items-center">
               <Link :href="route('dashboard')">
-                <img src="/img/beraksi.png" alt="Beraksi Logo" class="block h-9 w-auto" />
+              <img src="/img/beraksi.png" alt="Beraksi Logo" class="block h-9 w-auto" />
               </Link>
             </div>
           </div>
@@ -64,7 +64,7 @@ function isActive(routeName) {
                     <button type="button"
                       class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-200 bg-blue-700 hover:text-gray-400 focus:outline-none transition ease-in-out duration-150">
                       {{ user.name }}
-                      <font-awesome-icon :icon="faChevronDown" class="ml-1 text-xs"/>
+                      <font-awesome-icon :icon="faChevronDown" class="ml-1 text-xs" />
                     </button>
                   </span>
                 </template>
@@ -110,7 +110,7 @@ function isActive(routeName) {
       <!-- Bottom Navigation -->
       <header class="border bg-white shadow fixed bottom-0 left-0 right-0 z-40 h-12 sm:sticky sm:top-16 sm:h-auto">
         <div class="max-w-7xl mx-auto py-2 px-4 sm:px-6 lg:px-8 flex flex-col items-center justify-center">
-          <nav class="flex overflow-x-auto whitespace-nowrap w-full gap-8 justify-start">
+          <nav class="flex overflow-x-auto whitespace-nowrap scrollbar-hide w-full lg:gap-8 gap-3 justify-center">
             <template v-for="menu in menus" :key="menu.route">
               <Link :href="route(menu.route)" class="flex flex-col items-center text-sm sm:text-base" :class="{
                 'border-b-2 border-cyan-500': isActive(menu.route)
@@ -129,10 +129,13 @@ function isActive(routeName) {
 
     <div class="flex-1 pt-16 pb-12 sm:pt-16 sm:pb-0 relative">
       <div v-if="isLoading" class="z-30 fixed inset-0 flex items-center justify-center bg-white bg-opacity-70">
-          <svg class="animate-spin h-20 w-20 text-green-700" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-          </svg>
+        <svg class="animate-spin h-20 w-20 text-green-700" xmlns="http://www.w3.org/2000/svg" fill="none"
+          viewBox="0 0 24 24">
+          <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+          <path class="opacity-75" fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+          </path>
+        </svg>
       </div>
 
       <main class="h-full overflow-auto">
